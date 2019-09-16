548ad0729e3bf3b8da445efaaf68eca20172bb6d
kryptn <kryptn@gmail.com>
2019-09-15 23:59:16 -0600

    named layers, switched number row from top to bottom layer, now everything is shiftable on base

--- keyboards/planck/keymaps/kryptn/keymap.c
+++ keyboards/planck/keymaps/kryptn/keymap.c
@@ -33,9 +33,9 @@ enum planck_layers {
   _LOWER,
   _RAISE,
   _ADJUST,
-  _LAYER4,
-  _LAYER5,
-  _LAYER6,
+  _NAV,
+  _RMOUSE,
+  _NUMPAD,
 };
 
 #include "dynamic_macro.h"
@@ -51,16 +51,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_HYPR,         KC_LCTRL, KC_LALT, KC_LGUI, LOWER, KC_SPACE, KC_NO, RAISE, KC_LEFT,  KC_DOWN, KC_UP,     KC_RIGHT),
 
   [_LOWER] = LAYOUT_planck_grid(
-      KC_GRAVE, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,       KC_ASTR,             KC_LPRN,           KC_RPRN,         KC_DELETE,
+      KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,          KC_8,                KC_9,              KC_0,            KC_DELETE,
       _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINUS,      KC_EQUAL,            KC_LBRACKET,       KC_RBRACKET,     KC_PIPE,
       _______,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NONUS_HASH, KC_NONUS_BSLASH,     KC_HOME,           KC_END,          _______,
       _______,  _______, _______, _______, _______, _______, KC_NO,   _______,       KC_MEDIA_NEXT_TRACK, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_MEDIA_PLAY_PAUSE),
 
   [_RAISE] = LAYOUT_planck_grid(
-      KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,   KC_7,          KC_8,                KC_9,              KC_0,            KC_DELETE,
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,  KC_UNDS,       KC_PLUS,             KC_LCBR,           KC_RCBR,         KC_BSLASH,
-      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_NONUS_HASH, KC_NONUS_BSLASH,     KC_PGUP,           KC_PGDOWN,       _______,
-      _______, _______, _______, _______, _______, _______, KC_NO,  _______,       KC_MEDIA_NEXT_TRACK, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_MEDIA_PLAY_PAUSE),
+      KC_TILD, KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,       KC_ASTR,             KC_LPRN,           KC_RPRN,         KC_DELETE,
+      _______, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,       KC_PLUS,             KC_LCBR,           KC_RCBR,         KC_BSLASH,
+      _______, KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NONUS_HASH, KC_NONUS_BSLASH,     KC_PGUP,           KC_PGDOWN,       _______,
+      _______, _______,  _______, _______, _______, _______, KC_NO,   _______,       KC_MEDIA_NEXT_TRACK, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_MEDIA_PLAY_PAUSE),
 
   [_ADJUST] = LAYOUT_planck_grid(
       _______, _______, _______, _______, _______, DYN_REC_STOP,    _______,         _______, _______, _______, _______, _______,
@@ -68,19 +68,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______, _______, MU_ON,   MU_OFF,  MU_TOG,  DYN_MACRO_PLAY1, DYN_MACRO_PLAY2, RGB_MOD, RGB_HUI, RGB_HUD, _______, _______,
       MO(6),   _______, _______, _______, _______, _______,         KC_NO,           _______, _______, _______, _______, _______),
 
-  [_LAYER4] = LAYOUT_planck_grid(
+  [_NAV] = LAYOUT_planck_grid(
       _______, KC_MS_BTN1, KC_MS_UP,   KC_MS_BTN2,  KC_MS_WH_UP,   _______, _______, KC_PGUP, KC_UP,   KC_PGDOWN, _______, KC_DELETE,
       _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_WH_DOWN, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT,  _______, _______,
       _______, _______,    _______,    _______,     _______,       _______, KC_END,  _______, _______, _______,   _______, _______,
       _______, _______,    _______,    _______,     KC_MS_BTN1,    _______, KC_NO,   MO(5),   _______, _______,   _______, _______),
 
-  [_LAYER5] = LAYOUT_planck_grid(
+  [_RMOUSE] = LAYOUT_planck_grid(
       _______, _______, _______, _______, _______, _______, _______, KC_MS_BTN1, KC_MS_UP,   KC_MS_BTN2,  KC_MS_WH_UP,   _______,
       _______, _______, _______, _______, _______, _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_WH_DOWN, _______,
       _______, _______, _______, _______, _______, _______, _______, _______,    KC_MS_BTN3, _______,     _______,       _______,
       _______, _______, _______, _______, _______, _______, KC_NO,   _______,    _______,    _______,     _______,       _______),
 
-  [_LAYER6] = LAYOUT_planck_grid(
+  [_NUMPAD] = LAYOUT_planck_grid(
       _______, _______, _______, _______, _______, _______, _______, KC_7, KC_8,    KC_9,    KC_KP_SLASH,    _______,
       _______, _______, _______, _______, _______, _______, _______, KC_4, KC_5,    KC_6,    KC_KP_ASTERISK, _______,
       _______, _______, _______, _______, _______, _______, _______, KC_1, KC_2,    KC_3,    KC_KP_MINUS,    _______,
